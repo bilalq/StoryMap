447ef1e490e07608f00221d055c3a1f57f57ad9d
Bilal Quadri <bilalquadri92@gmail.com>
2012-02-25 14:23:00 -0800

    Did some styling

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
+
+<!--DOCUMENT HEAD-->
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>StoryMap - A new way to see the world</title>
-    <link rel="stylesheet" href="/stylesheets/main.css" type="text/css" charset="utf-8">
+
     <script type="text/javascript" src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0"></script>
+    <script type="text/javascript" src='javascripts/jquery/jquery-1.7.1.min.js'></script>
+
+    <link rel="stylesheet" href="/stylesheets/main.css" type="text/css" charset="utf-8">
+
     <script type="text/javascript">
       var map = null;
       function getMap() {
@@ -13,7 +19,14 @@
     </script>
 </head>
 
-<body onload="getMap();">
-  <div id='myMap' style="position:relative; width:400px; height:400px;"></div>
+<!--DOCUMENT BODY-->
+<body> 
+  <script>
+    $(document).ready(function(){
+      getMap(); //loads map
+    });
+  </script>
+  <div id='myMap'></div>
 </body>
+
 </html>
