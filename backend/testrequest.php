5670a3daf92d0da5130a70687fb13d69b11a379b
Bilal Quadri <bilalquadri92@gmail.com>
2012-02-25 19:57:04 -0800

    Fixed api call execution

--- backend/testrequest.php
+++ backend/testrequest.php
@@ -18,9 +18,10 @@ function geolocationlong()
 include("countrylist.php"); 
 $size = count($countrylist); 
 
-
 $i=0;
-while($i<count($size))
+
+
+while($i<$size)
 {
 
   $jsonurl = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=facet_terms%3A'.$countrylist[$i].'small_image%3Ay&fields=geo_facet%2Csmall_image_url%2Curl%2Ctitle%2Cbody&rank=newest&api-key=bb7933c4e64db04f027b97b683a82c81:13:65718622';
