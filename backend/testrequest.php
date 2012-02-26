b0de038fd2aa72781a4993d8d0c8f32a77ae3d3b
Bilal Quadri <bilalquadri92@gmail.com>
2012-02-25 19:39:58 -0800

    Fixed errors and indentation

--- backend/testrequest.php
+++ backend/testrequest.php
@@ -1,5 +1,20 @@
 <?php
 
+function geolocationlat()
+{
+  $geocode=file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address=' .$countrylist[$i]. '&sensor=false');
+  $output= json_decode($geocode);
+
+  return 	$output->results[$i]->geometry->location->lat;
+}
+function geolocationlong()
+{
+  $geocode=file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address=' .$countrylist[$i]. '&sensor=false');
+  $output= json_decode($geocode);
+
+  return 	 $output->results[$i]->geometry->location->lng;
+}
+
 include("countrylist.php"); 
 $size = count($countrylist); 
 
@@ -8,45 +23,29 @@ $i=0;
 while($i<count($size))
 {
 
-	$jsonurl = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=facet_terms%3A'.$countrylist[$i].'small_image%3Ay&fields=geo_facet%2Csmall_image_url%2Curl%2Ctitle%2Cbody&rank=newest&api-key=bb7933c4e64db04f027b97b683a82c81:13:65718622';
-	$json = file_get_contents($jsonurl);
-	$data = json_decode($json);
-	$numresults = count($data->results);
+  $jsonurl = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=facet_terms%3A'.$countrylist[$i].'small_image%3Ay&fields=geo_facet%2Csmall_image_url%2Curl%2Ctitle%2Cbody&rank=newest&api-key=bb7933c4e64db04f027b97b683a82c81:13:65718622';
+  $json = file_get_contents($jsonurl);
+  $data = json_decode($json);
+  $numresults = count($data->results);
+  echo $numresults;
 
-	function geolocationlat()
-	{
-		$geocode=file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address=' .$countrylist[$i]. '&sensor=false');
-		$output= json_decode($geocode);
-	
-		return 	$output->results[$i]->geometry->location->lat;
-	}
-		function geolocationlong()
-	{
-		$geocode=file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address=' .$countrylist[$i]. '&sensor=false');
-		$output= json_decode($geocode);
-	
-		return 	 $output->results[$i]->geometry->location->lng;
-	}
+  $j = 0;
+  while($j < $numresults)
+  {
+    $body = $data->results[$j]->body;
+    $url = $data->results[$j]->url;
+    $title = $data->results[$j]->title;
+    $small_image_url = $data->results[$j]->small_image_url;
+    $large_image_url = str_replace("thumbStandard", "articleLarge", $small_image_url);
+    $lat = geolocationlat();
+    $long = geolocationlong();
 
+    print $countrylist[$i];
+    print $url;
+    print $lat;
+    $j++;
+  }
 
-	$j = 0;
-	while($j < $numresults)
-	{
-		$body = $data->results[$j]->body;
-		$url = $data->results[$j]->url;
-		$title = $data->results[$j]->title;
-		$small_image_url = $data->results[$j]->small_image_url;
-		$large_image_url = str_replace("thumbStandard", "articleLarge", $small_image_url);
-		$lat = geolocationlat();
-		$long = geolocationlong();
-		
-		print $countrylist[$i];
-		print $url;
-		print $lat;
-		$j++;
-	}
-
-$i++;
-
+  $i++;
+  sleep(1);
 }
-
