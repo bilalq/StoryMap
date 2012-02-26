35ad8e82bb0502fd13a13bf174191c23208778e3
Bilal Quadri <bilalquadri92@gmail.com>
2012-02-25 17:24:57 -0800

    Fixed bugs

--- mongodb/parse.php
+++ mongodb/parse.php
@@ -1,8 +1,7 @@
 <?php
 $jsonurl = "http://api.nytimes.com/svc/search/v1/article?format=json&query=facet_terms%3Anew+york+small_image%3Ay&fields=geo_facet%2Csmall_image_url%2Ctitle%2Cbody&rank=newest&api-key=bb7933c4e64db04f027b97b683a82c81:13:65718622";
-$json = file_get_html($jsonurl);
-$data = json_decode($json,true);
+$json = file_get_contents($jsonurl);
+$data = json_decode($json);
 
-
-echo $data['results'[0]]['title']; 
-
+$test = $data->results[0]->body;
+echo $test;
