6dc13928cab6d4f65331f4bbb030116502e68c42
unknown <cj@cj-HP.(none)>
2012-02-25 22:21:01 -0500

    fixed count

--- backend/testrequest.php
+++ backend/testrequest.php
@@ -11,7 +11,7 @@ while($i<count($size))
 	$jsonurl = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=facet_terms%3A'.$countrylist[$i].'small_image%3Ay&fields=geo_facet%2Csmall_image_url%2Curl%2Ctitle%2Cbody&rank=newest&api-key=bb7933c4e64db04f027b97b683a82c81:13:65718622';
 	$json = file_get_contents($jsonurl);
 	$data = json_decode($json);
-	$numresults = $data->count(results);
+	$numresults = count($data->(results));
 
 	function geolocationlat()
 	{
