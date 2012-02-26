c005d30fa5d977b1cbcc4db83a327592735f6bcb
unknown <cj@cj-HP.(none)>
2012-02-26 03:55:42 -0500

    made arrays of same size

--- backend/spiral.php
+++ backend/spiral.php
@@ -74,8 +74,7 @@ while($i<1)
 
 
 
-      mysql_query("INSERT INTO 'storymap' 'data' ('id','source','headline','body','thumb','image','lat','long')
-      VALUES ('$idcount', '$url', '$title', '$small_image_url', '$large_image_url', '$lat', '$long')");
+      mysql_query("INSERT INTO data (id,source,headline,body,thumb,image,lat,long) VALUES ('$idcount', '$url', '$title', '$body' ,'$small_image_url', '$large_image_url', '$lat', '$long')");
       $idcount++;
 
         $j++;
